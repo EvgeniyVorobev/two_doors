f43a8fa72874e3ac9b44f41a46b82ba0651abb1c
Evgeniy Vorobev <evggordadym@gmail.com>
2019-10-22 15:31:00 +0300

    Update multiple_door.js

--- multiple_door.js
+++ multiple_door.js
@@ -3,19 +3,15 @@ $(function () {
     // Add event listeners to a elements!;
     $('a').filter(function (i, d) {
         if (d.href.indexOf('#glass') != -1) {
-            $glass = $glass.add(d);
             return true;
         }
         if (d.href.indexOf('#doorColor') != -1) {
-            $doorColor = $doorColor.add(d);
             return true;
         }
         if (d.href.indexOf('#doorInnerColor') != -1) {
-            $doorInnerColor = $doorInnerColor.add(d);
             return true;
         }
         if (d.href.indexOf('#doorOuterColor') != -1) {
-            $doorOuterColor = $doorOuterColor.add(d);
             return true;
         }
     })
